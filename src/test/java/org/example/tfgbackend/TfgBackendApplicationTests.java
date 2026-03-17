f786621ca69a9aa19f297e406244d276fa74fb52
sergi <sergio.negro@educa.madrid.org>
2026-03-17 10:57:30 +0100

    Primer commit

---
+++ src/test/java/org/example/tfgbackend/TfgBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tfgbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
