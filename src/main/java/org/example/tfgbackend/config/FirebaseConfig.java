3634e18e51486c2a259506de60b7a755348b9dc6
sergi <sergio.negro@educa.madrid.org>
2026-04-19 19:08:52 +0200

    Cambios para que funcione el backend sin errores fantasma

--- src/main/java/org/example/tfgbackend/config/FirebaseConfig.java
+++ src/main/java/org/example/tfgbackend/config/FirebaseConfig.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 import java.io.IOException;
 
 @Configuration
